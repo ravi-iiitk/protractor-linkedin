5c35cbbb281bc1bc330f7eceb04f822a8e28f796
Ravi Shankar Kumar <ravi.kumar@optimizeitsystems.com>
2019-03-23 11:52:20 +0530

    Fixed Linkedin job apply code.

--- page_objects/job-page.js
+++ page_objects/job-page.js
@@ -52,63 +52,60 @@ exports.jobpage={
     },
 
     applyJob : function () {
-        applyforJob();
+        var pageCounter = 1;
+        applyForJobEachPage(pageCounter);
     }
 };
-
-function applyforJob() {
-
-        var pageCounter = 1;
-        while(pageCounter<=40)
-        {
-             pageCounter = applyForJobEachPage(pageCounter);
-        }
-}
 
 
 function applyForJobEachPage(pageCounter) {
     var currentPage = element(by.xpath("//li[@class='artdeco-pagination__indicator artdeco-pagination__indicator--number active selected']/span"));
-    /*currentPage.getText().then(function (currentPage) {
-        });
-    console.log("Current Page is : Page No :-"+currentPage);*/
-    console.log("Page Counter is :-"+pageCounter);
-    if(pageCounter!==1 && pageCounter!==9  )
-    {
-        var xpath_page = "//button[@aria-label='Page "+pageCounter+"']";
-        var this_page = element(by.xpath(xpath_page));
-        commonlib.protractor_common.check_click(this_page,15);
-        browser.sleep(5000);
-        var firstJob = element.all(by.xpath("//li[contains(@id,'ember')]//h3[contains(@id,'ember')]/a")).get(0);
-        commonlib.protractor_common.check_click(firstJob,15);
-        browser.sleep(3000);
-    }
-    else if(pageCounter===9 )
-    {
-        var dotdot = element(by.xpath("//span[contains(text(),'…')]"));
-        commonlib.protractor_common.check_click(dotdot,15);
-        firstJob = element.all(by.xpath("//li[contains(@id,'ember')]//h3[contains(@id,'ember')]/a")).get(0);
-        commonlib.protractor_common.check_click(firstJob,15);
-        browser.sleep(5000);
-    }
-    scrollTillBottomLeft(1);
-    var all_job_heading = element.all(by.xpath("//li[contains(@id,'ember')]//h3[contains(@id,'ember')]/a"));
-    all_job_heading.count().then(function (no_jobs_list) {
-        console.log("Number of Jobs :"+no_jobs_list);
-        for(var job_counter=0;job_counter<no_jobs_list;job_counter++)
+    currentPage.getText().then(function (currentPage) {
+        console.log("Current Page is : Page No :-"+currentPage);
+        console.log("Page Counter is :-"+pageCounter);
+        if(pageCounter!==1 && pageCounter!==9  )
         {
-            browser.sleep(1000);
-            checkJobIsThere(job_counter);
+            var xpath_page = "//button[@aria-label='Page "+pageCounter+"']";
+            var this_page = element(by.xpath(xpath_page));
+            if(!this_page.isPresent())
+            {
 
+            }
+            commonlib.protractor_common.check_click(this_page,15);
+            browser.sleep(5000);
+            var firstJob = element.all(by.xpath("//li[contains(@id,'ember')]//h3[contains(@id,'ember')]/a")).get(0);
+            commonlib.protractor_common.check_click(firstJob,15);
+            browser.sleep(3000);
         }
+        else if(pageCounter===9 )
+        {
+            var dotdot = element(by.xpath("//span[contains(text(),'…')]"));
+            commonlib.protractor_common.check_click(dotdot,15);
+            firstJob = element.all(by.xpath("//li[contains(@id,'ember')]//h3[contains(@id,'ember')]/a")).get(0);
+            commonlib.protractor_common.check_click(firstJob,15);
+            browser.sleep(5000);
+        }
+        scrollTillBottomLeft(1);
+        var all_job_heading = element.all(by.xpath("//li[contains(@id,'ember')]//h3[contains(@id,'ember')]/a"));
+        all_job_heading.count().then(function (no_jobs_list) {
+            console.log("Number of Jobs :"+no_jobs_list);
+            for(var job_counter=0;job_counter<no_jobs_list;job_counter++)
+            {
+                browser.sleep(1000);
+                checkJobIsThere(pageCounter,job_counter);
+
+            }
+        });
     });
-
     pageCounter = pageCounter+1;
-    return pageCounter;
-
+    if(pageCounter<=40)
+        applyForJobEachPage(pageCounter);
 }
 
 
-function checkJobIsThere(job_counter) {
+
+
+function checkJobIsThere(pageCounter,job_counter) {
     var all_job_heading = element.all(by.xpath("//li[contains(@id,'ember')]//h3[contains(@id,'ember')]/a"));
     var this_heading = all_job_heading.get(job_counter);
     commonlib.protractor_common.check_click(this_heading,15);
@@ -119,12 +116,12 @@ function checkJobIsThere(job_counter) {
         {
             console.log("I am going inside the job match ");
             browser.sleep(1000);
-            locationCheckandApply(job_counter)
+            locationCheckandApply(pageCounter,job_counter)
         }
     })
 }
 
-function locationCheckandApply(job_counter) {
+function locationCheckandApply(pageCounter,job_counter) {
     var all_job_location = element.all(by.xpath("//li[contains(@id,'ember')]//h5[contains(@id,'ember')]"));
     var this_job_loc = all_job_location.get(job_counter);
     this_job_loc.getText().then(function (job_loc) {
@@ -160,7 +157,7 @@ function locationCheckandApply(job_counter) {
                                         }
                                         else
                                         {
-                                            applyOtherTab();
+                                            applyOtherTab(pageCounter);
                                         }
                                     });
                                 }
@@ -175,7 +172,7 @@ function locationCheckandApply(job_counter) {
     });
 }
 
-function applyOtherTab() {
+function applyOtherTab(pageCounter) {
     browser.getAllWindowHandles().then(function(handles) {
         browser.sleep(5000);
         var newTabHandle = handles[1];
@@ -188,7 +185,7 @@ function applyOtherTab() {
             browser.driver.close().then(function () {
                 browser.driver.switchTo().window(handles[0]).then(function () {
                     browser.sleep(15000);
-                    applyforJob();
+                    applyForJobEachPage(pageCounter);
                 });
             });
         });
